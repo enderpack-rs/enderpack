709dfcf7af2fedbdf05d633aa66e66f8c32b622c
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-05-29 12:34:26 +0200

    feat: exact ranges

--- src/data_types/range.rs
+++ src/data_types/range.rs
@@ -8,6 +8,7 @@ pub enum MCRange {
     Bound(Range<f64>),
     From(RangeFrom<f64>),
     To(RangeTo<f64>),
+    Exact(f64),
 }
 
 impl Display for MCRange {
@@ -16,6 +17,7 @@ impl Display for MCRange {
             MCRange::Bound(range) => format!("{}..{}", range.start, range.end),
             MCRange::From(range) => format!("{}..", range.start),
             MCRange::To(range) => format!("..{}", range.end),
+            MCRange::Exact(exact) => exact.to_string(),
         };
         write!(f, "{}", as_string)
     }
@@ -36,5 +38,11 @@ impl From<RangeFrom<f64>> for MCRange {
 impl From<RangeTo<f64>> for MCRange {
     fn from(value: RangeTo<f64>) -> Self {
         MCRange::To(value)
+    }
+}
+
+impl From<f64> for MCRange {
+    fn from(value: f64) -> Self {
+        MCRange::Exact(value)
     }
 }
