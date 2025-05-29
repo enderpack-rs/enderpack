a4bfa2a3dde096335d0d1596fc2bd445f723c2c3
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-05-29 14:31:00 +0200

    refactor

--- src/data_types/selector/argument.rs
+++ src/data_types/selector/argument.rs
@@ -1,18 +1,12 @@
 use std::fmt::Display;
 
 #[derive(Debug)]
-pub struct Argument<T>
-where
-    T: Display,
-{
+pub struct Argument<T: Display> {
     name: String,
     value: T,
 }
 
-impl<T> Argument<T>
-where
-    T: Display,
-{
+impl<T: Display> Argument<T> {
     pub fn new(name: &str, value: T) -> Self {
         Argument {
             name: name.to_owned(),
@@ -21,10 +15,7 @@ where
     }
 }
 
-impl<T> Display for Argument<T>
-where
-    T: Display,
-{
+impl<T: Display> Display for Argument<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}={}", self.name, self.value)
     }
