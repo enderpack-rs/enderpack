e8833effcc9600b30cddc382eb4f59980db2a1d7
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-01 13:09:10 +0200

    update registration example

--- examples/registration_builder.rs
+++ examples/registration_builder.rs
@@ -3,21 +3,23 @@ use serde_json::json;
 
 fn main() {
     let dp = Datapack::new(get_namespace!(), "My awesome datapack!", Version::new(71))
-        .add_function(enderpack::function::Tag::Load, load);
+        .add_function(Tag::Load, load);
     println!("{:#?}", dp);
 }
 
 #[func]
 fn load() {
     tellraw(all(), json!("hi!"));
-    effect().give(all(), resource::EffectResource::Speed);
+    effect().give(all(), resource::Speed);
+    scoreboard().objectives().add("test", resource::Dummy);
 }
 
 // will expand to
 // fn load() -> Function {
 //     Function::new(stringify!(load))
 //         .set_path(module_path!())
-//         .add_command(tellraw(all().distance(..5.0), json!("hi!")))
+//         .add_command(tellraw(all(), json!("hi!")))
+//         .add_command(effect().give(all(), resource::EffectResource::Speed))
 // }
 
 // `registration_builder` is an example, and Rust treats each example
