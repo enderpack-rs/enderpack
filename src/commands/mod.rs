1569949643de56b7f13bd4d3709f42466d5b53cd
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-01 11:23:09 +0200

    feat: subcommand_setup!()
    this macro avoids a lot of boilerplate when defining unit structs for
    subcommands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,3 +6,29 @@ pub use effect::*;
 pub use tellraw::*;
 
 pub trait Command: Display {}
+
+#[macro_export]
+macro_rules! subcommand_setup {
+    ($struct:ident => $function:ident { $($subcommand:ident => $substruct:ident),+ }) => {
+        impl $struct {
+            $(
+                pub fn $subcommand(self) -> $substruct {
+                    $substruct {}
+                }
+            )*
+        }
+
+        pub fn $function() -> $struct {
+            $struct {}
+        }
+    };
+    ($struct:ident { $($subcommand:ident => $substruct:ident),+ }) => {
+        impl $struct {
+            $(
+                pub fn $subcommand(self) -> $substruct {
+                    $substruct {}
+                }
+            )*
+        }
+    };
+}
--- src/prelude.rs
+++ src/prelude.rs
@@ -3,5 +3,6 @@ pub use crate::commands::*;
 pub use crate::data_types::*;
 pub use crate::function::*;
 pub use crate::get_namespace;
+pub use crate::subcommand_setup;
 
 pub use enderpack_macro::*;
