b50b8edd0e3fda6a127ac617552e70d20167da39
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-01 12:05:12 +0200

    refactor effect command

--- src/commands/effect/mod.rs
+++ src/commands/effect/mod.rs
@@ -2,16 +2,17 @@ pub mod give;
 
 use give::EffectGive;
 
-use crate::prelude::{Selector, resource::EffectResource};
+use crate::{
+    command_setup,
+    prelude::{Selector, resource::EffectResource},
+    subcommand_setup,
+};
 
 pub struct Effect;
 
-impl Effect {
-    pub fn give<T: Selector>(self, selector: T, effect: EffectResource) -> EffectGive<T> {
-        EffectGive::new(selector, effect)
-    }
-}
-
-pub fn effect() -> Effect {
-    Effect {}
-}
+command_setup!(Effect => effect);
+subcommand_setup!(Effect {
+    new with Selector {
+        give(selector: T, effect: EffectResource) => EffectGive
+    };
+});
