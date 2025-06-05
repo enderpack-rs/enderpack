024122beb1d6eaaca3e434e17f81f381d3badedb
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-05 23:14:52 +0200

    fix: arguments! macro yields "not declared in this scope"

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -55,7 +55,7 @@ macro_rules! subcommands {
 
 #[macro_export]
 macro_rules! arguments {
-    ($struct:ident => $as_string:literal {
+    ($struct:ident $(with $g_name:ident: $generic:ident)? => $as_string:literal {
         $(required {
             $($(#[$req_attr:meta])* $req_arg_name:ident: $req_arg_type:ty),+
         };)?
@@ -63,71 +63,34 @@ macro_rules! arguments {
             $($(#[$opt_attr:meta])* $opt_arg_name:ident: $opt_arg_type:ty),+
         };)?
     }) => {
-        pub struct $struct {
+        #[derive(::derive_new::new)]
+        pub struct $struct$(<$g_name: $generic>)? {
             $($(
                 $(#[$req_attr])*
-                $req_arg_name: $req_arg_type
-            ),+)?
+                $req_arg_name: $req_arg_type,
+            )+)?
             $($(
                 $(#[$opt_attr])*
                 #[new(default)]
-                $opt_arg_name: Option<$opt_arg_type>
-            ),+)?
+                $opt_arg_name: Option<$opt_arg_type>,
+            )+)?
         }
 
-        impl std::fmt::Display for $struct {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}", $as_string)?;
-                $($(
-                    write!(f, " {}", $req_arg_name)?;
-                ),+)?
-                $($(
-                    if let Some(some_$opt_arg_name) = $opt_arg_name {
-                        write!(f, " {}", some_$opt_arg_name)?;
-                    }
-                )+)?
-                Ok(())
-            }
-        }
-
-        impl $crate::commands::Command for $struct {}
-    };
-    ($struct:ident with $generic:ident => $as_string:literal {
-        $(required {
-            $($(#[$req_attr:meta])* $req_arg_name:ident: $req_arg_type:ty),+
-        };)?
-        $(optional {
-            $($(#[$opt_attr:meta])* $opt_arg_name:ident: $opt_arg_type:ty),+
-        };)?
-    }) => {
-        #[derive(derive_new::new)]
-        pub struct $struct<T: $generic> {
-            $($(
-                $(#[$req_attr])*
-                $req_arg_name: $req_arg_type
-            ),+)?
-            $($(
-                $(#[$opt_attr])*
-                #[new(default)]
-                $opt_arg_name: Option<$opt_arg_type>
-            ),+)?
-        }
-
-        impl<T: $generic> std::fmt::Display for $struct<T> {
+        impl$(<$g_name: $generic>)? ::std::fmt::Display for $struct$(<$g_name>)? {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "{}", $as_string)?;
                 $($(
                     write!(f, " {}", self.$req_arg_name)?;
                 )+)?
                 $($(
-                    if let Some(some_$opt_arg_name) = $opt_arg_name {
-                        write!(f, " {}", some_$opt_arg_name)?;
+                    if let Some($opt_arg_name) = &self.$opt_arg_name {
+                        write!(f, " {}", $opt_arg_name)?;
                     }
                 )+)?
                 Ok(())
             }
         }
 
-        impl<T: $generic> $crate::commands::Command for $struct<T> {}
+        impl$(<$g_name: $generic>)? $crate::commands::Command for $struct$(<$g_name>)? {}
     };
 }
