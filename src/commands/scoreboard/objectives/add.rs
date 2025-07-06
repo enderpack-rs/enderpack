0e72f0d556018d5484f10e238955b0260f6b3dc2
enderprism <76617157+enderprism@users.noreply.github.com>
2025-07-07 00:00:54 +0200

    cleanup code

--- src/commands/scoreboard/objectives/add.rs
+++ src/commands/scoreboard/objectives/add.rs
@@ -2,7 +2,6 @@ use crate::{arguments, data_types::resource};
 
 arguments!(ScoreboardObjectivesAdd => "scoreboard objectives add" {
     required {
-        #[new(into)]
         objective: resource::Objective,
         criteria: resource::Criteria
     };
