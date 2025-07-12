d39a3c77803cdf5fe685f9a686e3af39a465cc06
enderprism <76617157+enderprism@users.noreply.github.com>
2025-07-12 15:54:29 +0200

    update example

--- examples/registration_builder.rs
+++ examples/registration_builder.rs
@@ -11,23 +11,26 @@ fn main() {
 fn load() {
     tellraw(all(), json!("hi!"));
     effect().give(all(), resource::Effect::Speed);
+
     let test_scoreboard = resource::Objective::new("test").unwrap();
     scoreboard()
         .objectives()
-        .add(&test_scoreboard, &resource::Criteria::Dummy);
+        .add(&test_scoreboard, resource::Criteria::Dummy);
+
     scoreboard().players().display().number_format(
-        all_players(),
+        all().distance(..5.0).sort(Sort::Nearest).limit(10),
         &test_scoreboard,
         resource::NumberFormat::Styled(json!({"bold": true})),
     );
     scoreboard()
         .objectives()
         .set_display(resource::ScoreboardSlot::SidebarTeam(resource::Color::Red))
-        .objective(resource::Objective::new("test").unwrap());
+        .objective(&test_scoreboard);
     scoreboard().objectives().modify(
         &test_scoreboard,
         objectives::modify::Mode::DisplayAutoupdate(true),
     );
+
     let a1: Score = 0;
     let a2: Score = -10;
     let a3: Score = 9 + 10;
