8bf490ae3f4f652f447e8df911cee0cbc4bda66e
enderprism <76617157+enderprism@users.noreply.github.com>
2025-07-14 15:01:11 +0200

    update example

--- examples/registration_builder.rs
+++ examples/registration_builder.rs
@@ -9,36 +9,18 @@ fn main() {
 
 #[func]
 fn load() {
-    // tellraw(all(), json!("hi!"));
-    // effect().give(all(), resource::Effect::Speed);
-    //
-    // let test_scoreboard = resource::Objective::new("test").unwrap();
-    // scoreboard()
-    //     .objectives()
-    //     .add(&test_scoreboard, resource::Criteria::Dummy);
-    //
-    // scoreboard().players().display().number_format(
-    //     all().distance(..5.0).sort(Sort::Nearest).limit(10),
-    //     &test_scoreboard,
-    //     resource::NumberFormat::Styled(json!({"bold": true})),
-    // );
-    // scoreboard()
-    //     .objectives()
-    //     .set_display(resource::ScoreboardSlot::SidebarTeam(resource::Color::Red))
-    //     .objective(&test_scoreboard);
-    // scoreboard().objectives().modify(
-    //     &test_scoreboard,
-    //     objectives::modify::Mode::DisplayAutoupdate(true),
-    // );
-    //
-    // let a1: Score = 0;
-    // let a2: Score = 9 + 10;
     let a: Score = -10;
     let b: Score = a;
     let c: Score = &b + 12 + 6 + 15;
     // This is more optimised as the number is resolved at compile-time.
     let d: Score = &b + (12 + 6 + 15);
     let e: Score = 32 + c;
+    hello_world();
+}
+
+#[func]
+fn hello_world() {
+    tellraw(all(), json!("hello world"));
 }
 
 // `registration_builder` is an example, and Rust treats each example
